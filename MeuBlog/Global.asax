da1e646e7c94fabac7d97ae041294ab5d2908db3
lexel <lexlopes9@gmail.com>
2014-09-05 00:19:40 -0300

    Primeiro commit, adiciona post no banco

---
+++ MeuBlog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MeuBlog.MvcApplication" Language="C#" %>
---
+++ MeuBlog/Infra/NHibernateHelper.cs
@@ -0,0 +1,35 @@
+﻿using FluentNHibernate.Cfg;
+using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace MeuBlog.Infra
+{
+    public class NHibernateHelper
+    {
+        private static ISessionFactory factory = CriaSessionFactory();
+
+        private static ISessionFactory CriaSessionFactory()
+        {
+            Configuration cfg = new Configuration();
+            cfg.Configure();
+            return Fluently.Configure(cfg)
+                .Mappings(x => 
+                    x.FluentMappings.AddFromAssembly(
+                        Assembly.GetExecutingAssembly()
+                    )
+                ).BuildSessionFactory();
+        }
+
+        public static ISession AbreSession()
+        {
+            return factory.OpenSession();
+                 
+        }
+
+    }
+}
