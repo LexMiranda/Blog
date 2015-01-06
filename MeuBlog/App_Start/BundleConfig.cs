b2f27c5d0fc434c8393886b4da409e7453962686
lexel <lexlopes9@gmail.com>
2015-01-06 12:53:08 -0200

    Optimização usando Bunble

---
+++ MeuBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MeuBlog.App_Start
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css")
+                .Include("~/Content/Css/bootstrap.css")
+                .Include("~/Content/blog.css"));
+        }
+    }
+}
--- MeuBlog/Global.asax.cs
+++ MeuBlog/Global.asax.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using MeuBlog.App_Start;
 
 namespace MeuBlog
 {
@@ -13,6 +15,7 @@ namespace MeuBlog
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
